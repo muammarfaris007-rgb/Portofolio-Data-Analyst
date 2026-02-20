19ae4017a23b21c62124d6698800b1627941cd74
muammarfaris007-rgb <muammarfaris007@gmail.com>
2026-02-21 00:26:28 +0700

    Update 1.importing-data-and-inspection.py

--- Data Analyst Project_ Business Decision Research/1.importing-data-and-inspection.py
+++ Data Analyst Project_ Business Decision Research/1.importing-data-and-inspection.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df = pd.read_csv('https://dqlab-dataset.s3-ap-southeast-1.amazonaws.com/data_retail.csv', sep=';')
+df = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/data_retail.csv', sep = ';')
 
 print('Lima data teratas:')
 print(df.head())
