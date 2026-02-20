9f1eb4855b48d42dbab85423ad372ff5e6bd1686
muammarfaris007-rgb <muammarfaris007@gmail.com>
2026-02-21 00:27:32 +0700

    Update 3.churn-customers.py

--- Data Analyst Project_ Business Decision Research/3.churn-customers.py
+++ Data Analyst Project_ Business Decision Research/3.churn-customers.py
@@ -1,3 +1,8 @@
+import pandas as pd
+df = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/data_retail.csv', sep=';')
+df['First_Transaction'] = pd.to_datetime(df['First_Transaction']/1000, unit='s', origin='1970-01-01')
+df['Last_Transaction'] = pd.to_datetime(df['Last_Transaction']/1000, unit='s', origin='1970-01-01')
+
 # Pengecekan transaksaksi terakhir dalam dataset
 print(max(df['Last_Transaction']))
 
