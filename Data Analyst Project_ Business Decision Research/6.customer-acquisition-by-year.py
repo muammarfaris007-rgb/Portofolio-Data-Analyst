e7597a5539890d5c108500b5819e110a00b2684e
muammarfaris007-rgb <muammarfaris007@gmail.com>
2026-02-21 00:28:55 +0700

    Update 6.customer-acquisition-by-year.py

--- Data Analyst Project_ Business Decision Research/6.customer-acquisition-by-year.py
+++ Data Analyst Project_ Business Decision Research/6.customer-acquisition-by-year.py
@@ -1,3 +1,8 @@
+import pandas as pd
+df = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/data_retail.csv', sep=';')
+df['First_Transaction'] = pd.to_datetime(df['First_Transaction']/1000, unit='s', origin='1970-01-01')
+df['Last_Transaction'] = pd.to_datetime(df['Last_Transaction']/1000, unit='s', origin='1970-01-01')
+
 import matplotlib.pyplot as plt
 
 # Kolom tahun transaksi pertama
@@ -6,7 +11,7 @@ df['Year_First_Transaction'] = df['First_Transaction'].dt.year
 df['Year_Last_Transaction'] = df['Last_Transaction'].dt.year
 
 df_year = df.groupby(['Year_First_Transaction'])['Customer_ID'].count()
-df_year.plot(x='Year_First_Transaction', y='Customer_ID', kind='bar', title='Graph of CUstomer Acquisition')
+df_year.plot(x='Year_First_Transaction', y='Customer_ID', kind='bar', title='Graph of Customer Acquisition')
 plt.xlabel('Year_First_Transaction')
 plt.ylabel('Num_of_Customer')
 plt.tight_layout()
