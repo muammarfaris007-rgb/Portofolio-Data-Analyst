4b378f74780080c2fcf1e1c2547b7f5a3581e70e
muammarfaris007-rgb <muammarfaris007@gmail.com>
2026-02-21 00:31:42 +0700

    Update 12.columns-and-target-features.py

--- Data Analyst Project_ Business Decision Research/12.columns-and-target-features.py
+++ Data Analyst Project_ Business Decision Research/12.columns-and-target-features.py
@@ -1,3 +1,12 @@
+import pandas as pd
+df = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/data_retail.csv', sep=';')
+df['First_Transaction'] = pd.to_datetime(df['First_Transaction']/1000, unit='s', origin='1970-01-01')
+df['Last_Transaction'] = pd.to_datetime(df['Last_Transaction']/1000, unit='s', origin='1970-01-01')
+df['Year_First_Transaction'] = df['First_Transaction'].dt.year
+df['Year_Last_Transaction'] = df['Last_Transaction'].dt.year
+df.loc[df['Last_Transaction'] <= '2018-08-01', 'is_churn'] = True 
+df.loc[df['Last_Transaction'] > '2018-08-01', 'is_churn'] = False 
+
 # Feature column: Year_Diff
 df['Year_Diff'] = df['Year_Last_Transaction'] - df['Year_First_Transaction']
 
@@ -8,4 +17,4 @@ feature_columns = ['Average_Transaction_Amount', 'Count_Transaction', 'Year_Diff
 X = df[feature_columns] 
 
 # Target variable
-y = df['is_churn'] 
+y = df['is_churn']
